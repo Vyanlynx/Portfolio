448e21d7f729301785b0d5aed4f793d42641a210
RAGHUL-R01 <raghulnishanth195@gmail.com>
2023-09-23 19:48:28 +0530

    Added Btn animation and Social media added

--- components/Main/MainContent.tsx
+++ components/Main/MainContent.tsx
@@ -5,9 +5,15 @@ import Insta from '../../public/FrontPageIcons/insta.svg'
 import Indeed from '../../public/FrontPageIcons/indeed.svg'
 import Discord from '../../public/FrontPageIcons/discord.svg'
 import MainImg from '../../public/mainImg.png'
+import { useEffect, useState } from "react";
 
 export default function MainContent(props: any) {
-
+    // const [devState, setDevState] = useState('');
+    // useEffect(() => {
+    //     props.stack.map((element: any) => {
+    //         setDevState(element)
+    //     })
+    // },[])
     return (
         <div className="text-white flex justify-center items-center contmain">
             <section className="cont1">
@@ -18,18 +24,19 @@ export default function MainContent(props: any) {
                             <span className="nameclr text-4xl">{props.name}</span>
                         </div>
                     </section>
-                    <div>{props.stack}</div>
+                    <div>{props.stack[1]}</div>
                 </div>
                 <div className="stack90">{props.summary}</div>
                 <div className="py-1">
-                    <button className="btn_talk">{props.connect}</button>
+                    <button className="shine-box">{props.connect}</button>
                 </div>
-                {/* <div className="socialMedia45 flex gap-2 justify-center items-center">
-                    <Image src={LinkedIn} height={30} width={30} alt="icon_socialMedia" />
-                    <Image src={Insta} height={30} width={30} alt="icon_socialMedia" />
-                    <Image src={Discord} height={25} width={25} alt="icon_socialMedia" />
-                    <Image src={Indeed} height={25} width={25} alt="icon_socialMedia" />
-                </div> */}
+                <div className="socialMedia45 flex gap-5 items-center">
+                    <div style={{ fontSize: "14px" }}>{props.socialMedia}</div>
+                    <Image src={LinkedIn} height={25} width={25} alt="icon_socialMedia" title="LinkedIn"/>
+                    <Image src={Insta} height={25} width={25} alt="icon_socialMedia" title="Instagram"/>
+                    <Image src={Discord} height={28} width={28} alt="icon_socialMedia" title="Discord"/>
+                    <Image src={Indeed} height={23} width={23} alt="icon_socialMedia" title="Indeed"/>
+                </div>
             </section>
             <section className="cont2">
                 {/* <Image src={MainImg} height={350} alt="dev"/> */}
