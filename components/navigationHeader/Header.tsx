4fcfb4850b46ce471a76c3b0c7d97b052a0ae822
RAGHUL-R01 <raghulnishanth195@gmail.com>
2023-10-16 23:47:43 +0530

    Hide show

--- components/navigationHeader/Header.tsx
+++ components/navigationHeader/Header.tsx
@@ -7,13 +7,15 @@ import { useRouter } from 'next/navigation';
 
 const HeaderComponent = (): JSX.Element => {
     const [inputVal, setInputVal] = useState<string>('');
+    const [showInputVal, setshowInputVal] = useState<any>(false);
     const router = useRouter()
-    const verFunction = (e:any) => {
+    let errorMsg = false
+    const verFunction = (e: any) => {
         setInputVal(e.target.value)
         let date = new Date();
         let pass = `v${date.getHours()}${date.getMinutes()}${date.getDay()}!@#$%`
         console.log(pass)
-        if (inputVal === pass) {
+        if (inputVal === pass && inputVal.length > 5) {
             if (window) window?.sessionStorage?.setItem('tokenz', pass)
             router.push('/resume')
         } else {
@@ -31,15 +33,15 @@ const HeaderComponent = (): JSX.Element => {
             <section className='flex justify-around items-center gap-20 text-sm'>
                 <nav>Projects</nav>
                 <nav className='res'>
-                    <div>Resume</div>
-                    <div className='tabbar'>
+                    <div onClick={() => setshowInputVal(!showInputVal)}>Resume</div>
+                    {showInputVal && <div className='tabbar'>
                         <div className='tabpop'>
-                            <div style={{ color: "white", paddingBottom: "0.3em" }}>Enter the key:</div>
+                            <div style={{ color: "white", paddingBottom: "0.3em" }}>Enter the key: {errorMsg ? "INCORRECT" : ''}</div>
                             <div style={{ display: "flex" }}>
                                 <input value={inputVal} onChange={verFunction} />
                             </div>
                         </div>
-                    </div>
+                    </div>}
                 </nav>
                 <nav>GitHub</nav>
                 <nav>Contact</nav>
