a932cd1f3fb2a7af9994299ad2aa1e14fcdda877
RAGHUL-R01 <raghulnishanth195@gmail.com>
2023-10-22 15:11:26 +0530

    new resume

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,26 +1,26 @@
 'use client'
-import { useEffect, useState } from "react"
+// import { useEffect, useState } from "react"
 import { useRouter } from 'next/navigation';
 
 export default function Page() {
     const router = useRouter();
-    const [loader, setLoader] = useState<boolean>(true)
-    useEffect(() => {
-        if (window) {
-            let date = new Date();
-            let pass = `v${date.getHours()}${date.getMinutes()}${date.getDay()}!@#$%`
-            let test = window.sessionStorage.getItem('tokenz')
-            if (test === pass) {
-                setLoader(false)
-            } else {
-                setTimeout(() => router.push('/'), 3000)
-            }
-        }
-    }, [])
+    // const [loader, setLoader] = useState<boolean>(true)
+    // useEffect(() => {
+    //     if (window) {
+    //         let date = new Date();
+    //         let pass = `v${date.getHours()}${date.getMinutes()}${date.getDay()}!@#$%`
+    //         let test = window.sessionStorage.getItem('tokenz')
+    //         if (test === pass) {
+    //             setLoader(false)
+    //         } else {
+    //             setTimeout(() => router.push('/'), 3000)
+    //         }
+    //     }
+    // }, [])
     return (
         <div style={{ height: "100vh" }}>
-            {loader && <div>Session expired! Login Again to continue...</div>}
-            {!loader && <object data="Raghul_CV.pdf" type="application/pdf" width="100%" height="100%"></object>}
+            {/* {loader && <div>Session expired! Login Again to continue...</div>} */}
+            <object data="Raghul_CV.pdf" type="application/pdf" width="100%" height="100%"></object>
         </div>
     )
 }
--- components/navigationHeader/Header.tsx
+++ components/navigationHeader/Header.tsx
@@ -10,18 +10,18 @@ const HeaderComponent = (): JSX.Element => {
     const [showInputVal, setshowInputVal] = useState<any>(false);
     const router = useRouter();
     let errorMsg = false
-    const verFunction = (e: any) => {
-        setInputVal(e.target.value)
-        let date = new Date();
-        let pass = `v${date.getHours()}${date.getMinutes()}${date.getDay()}!@#$%`
-        console.log(pass)
-        if (inputVal === pass && inputVal.length > 5) {
-            if (window) window?.sessionStorage?.setItem('tokenz', pass)
-            router.push('/resume')
-        } else {
-            console.log("TRY AGAIN")
-        }
-    }
+    // const verFunction = (e: any) => {
+    //     setInputVal(e.target.value)
+    //     let date = new Date();
+    //     let pass = `v${date.getHours()}${date.getMinutes()}${date.getDay()}!@#$%`
+    //     console.log(pass)
+    //     if (inputVal === pass && inputVal.length > 5) {
+    //         if (window) window?.sessionStorage?.setItem('tokenz', pass)
+    //         router.push('/resume')
+    //     } else {
+    //         console.log("TRY AGAIN")
+    //     }
+    // }
     return <>
         <div className='text-white flex justify-around items-center py-10' style={{marginLeft:"-5em"}}>
             <header className='cursor-pointer'>
@@ -33,15 +33,16 @@ const HeaderComponent = (): JSX.Element => {
             <section className='flex justify-around items-center gap-20 text-sm'>
                 <nav><a href='#Projects'>Projects</a></nav>
                 <nav className='res'>
-                    <div onClick={() => setshowInputVal(!showInputVal)}>Resume</div>
-                    {showInputVal && <div className='tabbar'>
+                    <div onClick={()=>router.push('/resume')}>Resume</div>
+                    {/* <div onClick={() => setshowInputVal(!showInputVal)}>Resume</div> */}
+                    {/* {showInputVal && <div className='tabbar'>
                         <div className='tabpop'>
                             <div style={{ color: "white", paddingBottom: "0.3em" }}>Enter the key: {errorMsg ? "INCORRECT" : ''}</div>
                             <div style={{ display: "flex" }}>
                                 <input value={inputVal} onChange={verFunction} />
                             </div>
                         </div>
-                    </div>}
+                    </div>} */}
                 </nav>
                 <nav><a href='https://github.com/Vyanlynx' target='_blank'>GitHub</a></nav>
                 {/* <nav> <a href='' target='_blank'>Contact</a></nav> */}
